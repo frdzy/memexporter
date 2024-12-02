e3a4cec51d0a5a3dfee14a56ae7cb1a1b60852c8
Fred Zhao <829693+frdzy@users.noreply.github.com>
2024-12-02 11:05:00 -0800

    Server processing for csv

--- api/src/services/imports.ts
+++ api/src/services/imports.ts
@@ -1,6 +1,21 @@
 import type { MutationResolvers } from 'types/graphql';
+import fs from 'fs';
+import Papa from 'papaparse';
 
-export const createImport: MutationResolvers['createImport'] = ({ input }) => {
+export const createImport: MutationResolvers['createImport'] = async ({
+  input,
+}) => {
+  const { files } = input;
   console.log('input', input);
+
+  const allPromises = files.map(async (file) => {
+    const text = await file.text();
+    const results = Papa.parse(text, { header: true });
+    return results.data;
+  });
+  const results = await Promise.allSettled(allPromises);
+  const data = results.map((r) => (r.status === 'fulfilled' ? r.value : null));
+  console.log('data', data);
+
   return true;
 };
